e169eb7cd652f8a53438cf25c40c4c5dd5899390
Sanjay <sanjayrydv@gmail.com>
2023-03-23 01:05:17 +0530

    Added categories

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const sliderItems =[
+    {
+        id: 1,
+        img: "https://img.freepik.com/free-photo/portrait-young-beautiful-brunette-woman-girl-model-with-nude-makeup-colorful-summer-pink-hipster-clothes-posing-near-wall-looking-happily_158538-8831.jpg?w=1480&t=st=1679508804~exp=1679509404~hmac=a9db430cc1ba409d421217f9d58d5b2b3e2c65b877c9f7d9cdf376c2eceeff3e",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://img.freepik.com/free-photo/brunette-lady-black-sportswear-tired-after-long-training-with-white-towel_231208-3439.jpg?w=900&t=st=1679508895~exp=1679509495~hmac=3184e14a5cf5b9fff96c68e6cc9414d6407095423efc3113e9adb2418ef9f9f4",
+        title: "GYM SALE",
+        desc: "KEEP YOUR HEAD HIGH, YOUR GLUTES TIGHT & YOUR COFFEE STRONG.",
+        bg: "fcf1ed",
+    }
+];
+
+export const categories = [
+    {
+       id: 1,
+       img: "https://img.freepik.com/free-photo/upset-woman-with-dark-hair-standing-light-wall_197531-6471.jpg?w=1480&t=st=1679513173~exp=1679513773~hmac=df37411ec5df64836a38b3c0a944d1792ae8a82880b80b759e82e8b2a49b9e1b",
+       title: "SHIRT STYLE!",
+    },
+    {
+       id: 2,
+       img: "https://www.thehomeworker.com/wp-content/uploads/2022/02/DSCF0737.jpg",
+       title: "LOUNGEWEAR LOVE",
+    },
+    {
+       id: 3,
+       img: "https://img.freepik.com/free-photo/stylish-woman-leather-coat-black-hat-demonstrate-winter-fashion-trends-white_273443-4937.jpg?w=1480&t=st=1679512953~exp=1679513553~hmac=ce29ab4452f13546396965dce1191c6edb03250040492cf880be7fc283fc1465",
+       title: "JACKETS",
+    },
+]
--- src/pages/components/Slider.jsx
+++ src/pages/components/Slider.jsx
@@ -1,15 +1,23 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import { useState } from "react";
 import styled from "styled-components";
+import { sliderItems } from "../../data";
+import girl from "./images/cake.png"
 
 const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
     position: relative;
+    overflow: hidden;
 `
 
 const Wrapper = styled.div`
-    height: 100%
+    height: 100%;
+    display: flex;
+    transition: all 1s ease;
+    transform: translateX(${(props)=>props.slideIndex * -100}vw)
+
 `
 const Arrow = styled.div`
     width: 50px;
@@ -27,14 +35,78 @@ const Arrow = styled.div`
     margin: auto;
     cursor: pointer;
     opacity: 0.6;
+    z-index: 2;
 `
+
+const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    background-color: #${props=>props.bg}
+`
+
+const Image = styled.img`
+    height: 80%;
+    margin-left: 50px;
+`
+const ImgContainer = styled.div`
+    height:100%;
+    flex:1;
+`
+const InfoContainer = styled.div`
+    flex:1;
+    padding: 50px;
+`
+
+const Title = styled.h1`
+    font-size: 70px;
+`
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+
+`
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
+`
+
+
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick =(direction)=>{
+    if(direction==="left"){
+        setSlideIndex(slideIndex > 0 ? slideIndex-1 : 1)
+    } else {
+        setSlideIndex(slideIndex < 1 ? slideIndex+1 : 0)
+    }
+  }
+  
   return (
     <Container>
-            <Arrow direction="left">
+            <Arrow direction="left" onClick={()=>handleClick('left')}>
                 <ArrowLeftOutlined/>
             </Arrow>
-            <Arrow direction="right">
+            <Wrapper slideIndex={slideIndex}>
+                {sliderItems.map(item=>(
+                    <Slide bg={item.bg}>
+                        <ImgContainer>
+                            <Image src={item.img} />
+                        </ImgContainer>
+                        <InfoContainer>
+                            <Title>{item.title}</Title>
+                            <Desc>{item.desc}</Desc>
+                            <Button>SHOP NOW</Button>
+                        </InfoContainer>
+                    </Slide>
+                    ))}
+            </Wrapper>
+            <Arrow direction="right" onClick={()=>handleClick('right')}>
                 <ArrowRightOutlined/>
             </Arrow>
     </Container>
