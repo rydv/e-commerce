e1db94dbbdeda5387f915283fa14ee18e7a02ba8
Sanjay Yadav <sanjayyadav@Sanjays-MacBook-Pro.local>
2023-03-25 16:20:21 +0530

    Finished home page

--- src/pages/components/Announcement.jsx
+++ src/pages/components/Announcement.jsx
@@ -15,7 +15,7 @@ const Container =styled.div`
 const Announcement = () => {
   return (
     <Container>
-        Super Deal! Free Shipping on Orders Over $50.
+        Super Deal! Free Shipping on Orders Over ₹1000.
     </Container>
   )
 }
