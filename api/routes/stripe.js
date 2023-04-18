2d82e077ae32f0253ab17ada57f5f687de3b006d
Sanjay <sanjayrydv@gmail.com>
2023-04-19 01:51:10 +0530

    env removed

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -2,17 +2,26 @@ const router = require("express").Router();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 
-router.post("/payment", (req,res) => {
-    stripe.charges.create({
-        source: req.body.tokenId,
-        amount: req.body.amount,
-        currency: "inr",
-    }, (stripeErr, stripeRes)=>{
-        if(stripeErr){
-            res.status(500).json(stripeErr);
-        }else{
-            res.status(200).json(stripeRes);
-        }
-    });
+// const stripe_key = process.env.STRIPE_KEY;
+
+
+
+router.post("/payment", (req, res) => {
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "usd",
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
+        console.log('Fuck you')
+      } else {
+        res.status(200).json(stripeRes);
+      }
+    }
+  );
 });
+
 module.exports = router;
